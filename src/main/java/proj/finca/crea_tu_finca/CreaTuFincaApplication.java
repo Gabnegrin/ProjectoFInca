27e57738606aa46069d40c9212121432bf76a40c
Gabnegrin <gabrielandres.negrinh@gmail.com>
2024-03-05 11:46:17 -0500

    push

---
+++ src/main/java/proj/finca/crea_tu_finca/CreaTuFincaApplication.java
@@ -0,0 +1,13 @@
+package proj.finca.crea_tu_finca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreaTuFincaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreaTuFincaApplication.class, args);
+	}
+
+}
